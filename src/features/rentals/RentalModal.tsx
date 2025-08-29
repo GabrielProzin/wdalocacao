14b851fbc57d4df1aad633534d9dd496db59c0fe
gabrielprozin <gabrielalexandrino2004@gmail.com>
2025-08-28 22:28:33 -0300

    Initializing AluguelForm migration to RentalModal

--- src/features/rentals/RentalModal.tsx
+++ src/features/rentals/RentalModal.tsx
@@ -3,14 +3,37 @@
 import { useEffect, useRef } from 'react';
 import styles from './rentals.module.css';
 
+import { Aluguel } from '@/features/aluguel/models/Aluguel';
+import { useAluguelForm } from '@/features/aluguel/hooks/useAluguelForm';
+import { montserrat, openSans, poppins } from '@/fonts/fonts';
+import { formatarDistanciaLegivel } from '@/utils/aluguelUtils';
+import {
+  formatarNome,
+  formatarTelefone,
+} from '@/features/validationFunctions/validationFunctions';
+
+type RentalModalProps = {
+  open: boolean;
+  onClose: () => void;
+  aluguel?: Aluguel;
+  modo?: 'cadastro' | 'editar';
+};
+
 export default function RentalModal({
   open,
   onClose,
-}: {
-  open: boolean;
-  onClose: () => void;
-}) {
+  aluguel,
+  modo = 'cadastro',
+}: RentalModalProps) {
   const panelRef = useRef<HTMLDivElement>(null);
+
+  const{
+    form,
+    setForm,
+    handleSubmit,
+    calcularValor,
+    mensagem,
+  } useAluguelForm(aluguel, modo)
 
   useEffect(() => {
     function onKey(e: KeyboardEvent) {
@@ -31,6 +54,17 @@ export default function RentalModal({
   }, [open]);
 
   if (!open) return null;
+
+  const jogosNumero = parseInt(form.jogos, 10);
+  const forrosNumero = parseInt(form.forroQuantidade, 10);
+  const valorFreteNumero = parseInt(form.valorFrete, 10);
+  const distanciaMetros = parseInt(form.distanciaKM, 10);
+
+  const onConfirmar = async (e: React.FormEvent) => {
+    e.preventDefault()
+    await handleSubmit(e)
+  }
+
 
   return (
     <div
